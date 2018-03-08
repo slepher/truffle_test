116c756c4d38d0c04a4da80ff2d09ddcb6bb2bde
slepher <slepheric@gmail.com>
2018-03-09 00:31:52 +0800

    update testcase

--- migrations/2_deploy_storage.js
+++ migrations/2_deploy_storage.js
@@ -1,5 +1,4 @@
 var StorageManager = artifacts.require("./StorageManager.sol");
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(StorageManager);
